3d6853bc8f60b9ba805bedc35efc5b2499ea34cd
Craig Mills <Craig.mills@unep-wcmc.org>
2011-05-23 22:45:06 +0100

    Initial Commit

---
+++ opencv_with_postits/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  opencv_with_postits
+//
+//  Created by Craig Mills on 23/05/2011.
+//  Copyright 2011 UNEP-WCMC. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main (int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
