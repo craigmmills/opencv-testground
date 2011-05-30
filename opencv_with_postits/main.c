d032fbca93cfa9aeb3b42d834764348d5517b527
Craig Mills <Craig.mills@unep-wcmc.org>
2011-05-30 14:34:47 +0100

    attempting to pull out patterns in individual planes using a histogram

--- opencv_with_postits/main.c
+++ opencv_with_postits/main.c
@@ -77,10 +77,107 @@ void draw_stuff(IplImage* img)
 }
 
 
+//try a different technique using obvious clumps of colour in the colour histogram
+void createHistogram(IplImage* src)
+{
+    
+    // Compute the HSV image and decompose it into separate planes.
+    //
+    IplImage* hsv = cvCreateImage( cvGetSize(src), 8, 3 );
+    cvCvtColor( src, hsv, CV_BGR2HSV );
+    IplImage* h_plane = cvCreateImage( cvGetSize(src), 8, 1 );
+    IplImage* s_plane = cvCreateImage( cvGetSize(src), 8, 1 );
+    IplImage* v_plane = cvCreateImage( cvGetSize(src), 8, 1 );
+    IplImage* planes[] = { h_plane, s_plane };
+    
+        
+    cvCvtPixToPlane( hsv, h_plane, s_plane, v_plane, 0 );
+    //draw_stuff(hsv);
+    //draw_stuff(h_plane);
+    draw_stuff(s_plane);
+    //draw_stuff(v_plane);
+    
+    // Build the histogram and compute its contents.
+    //
+    int h_bins = 30, s_bins = 32;
+    
+    CvHistogram* hist;
+    {
+        int hist_size[] = { h_bins, s_bins };
+        float h_ranges[] = { 0, 180 }; // hue is [0,180]
+        float s_ranges[] = { 0, 255 };
+        float* ranges[] = { h_ranges, s_ranges };
+        hist = cvCreateHist(
+                            2,
+                            hist_size,
+                            CV_HIST_ARRAY,
+                            ranges,
+                            1
+                            );
+    }
+    
+    
+    cvCalcHist( planes, hist, 0, 0 ); //Compute histogram
+    
+    
+    cvNormalizeHist( hist, 1.0 ); //Normalize it
+    // Create an image to use to visualize our histogram.
+    //
+    int scale = 10;
+    IplImage* hist_img = cvCreateImage(
+                                       cvSize( h_bins * scale, s_bins * scale ),
+                                       8,
+                                       3
+                                       );
+    cvZero( hist_img );
+    // populate our visualization with little gray squares.
+    //
+    float max_value = 0;
+    cvGetMinMaxHistValue( hist, 0, &max_value, 0, 0 );
+    
+    for( int h = 0; h < h_bins; h++ ) {
+        for( int s = 0; s < s_bins; s++ ) {
+            float bin_val = cvQueryHistValue_2D( hist, h, s );
+            int intensity = cvRound( bin_val * 255 / max_value );
+            cvRectangle(
+                        hist_img,
+                        cvPoint( h*scale, s*scale ),
+                        cvPoint( (h+1)*scale - 1, (s+1)*scale - 1),
+                        CV_RGB(intensity,intensity,intensity),
+                        CV_FILLED,8,0
+                        );
+        }
+    }
+    
+    cvNamedWindow( "orig", 1 );
+    
+    cvShowImage( "orig", src );
+    cvNamedWindow( "H-S Histogram", 1 );        
+    cvShowImage( "H-S Histogram", hist_img );
+    cvWaitKey(0);    
+}
+
+
 // returns sequence of squares detected on the image.
 // the sequence is stored in the specified memory storage
 CvSeq* findSquares4( IplImage* img, CvMemStorage* storage )
 {
+    
+    
+    //look at only one plane to get the squares:
+    // Compute the HSV image and decompose it into separate planes.
+    //
+    IplImage* hsv = cvCreateImage( cvGetSize(img), 8, 3 );
+    cvCvtColor( img, hsv, CV_BGR2HSV );
+    IplImage* h_plane = cvCreateImage( cvGetSize(img), 8, 1 );
+    IplImage* s_plane = cvCreateImage( cvGetSize(img), 8, 1 );
+    IplImage* v_plane = cvCreateImage( cvGetSize(img), 8, 1 );
+    cvCvtPixToPlane( hsv, h_plane, s_plane, v_plane, 0 );
+    draw_stuff(s_plane);
+    
+    //img = s_plane;
+
+    
     CvSeq* contours;
     int i, c, l, N = 11;
     CvSize sz = cvSize( img->width & -2, img->height & -2 );
@@ -283,6 +380,11 @@ int main(int argc, char** argv)
             break;
         }
         img = cvCloneImage( img0 );
+        
+        //create histogram of saturation to take a look at the distribution- see if we can identify patterns
+        
+        //createHistogram(img);
+        
         // force the image processing
         on_trackbar(0);
         // wait for key.
